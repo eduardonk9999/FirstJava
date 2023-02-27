9f814a699b4df26e70f9b99899d84dee5687d386
eduardonk9999 <thiago.novak7@gmail.com>
2023-02-27 16:56:03 -0300

    dataTypes

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,17 @@ public class Main {
         String name = "Eduardo";
         Date date = new Date();
 
+        // Data Types
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+
+       
+
+
     }
 }
