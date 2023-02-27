4fe67e49ad6efe1a9420b30b4540b4b4253983aa
eduardonk9999 <thiago.novak7@gmail.com>
2023-02-27 16:52:09 -0300

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int number = 100;
+        double numberTow = 10.02;
+        String name = "Eduardo";
+        Date date = new Date();
+
     }
 }
